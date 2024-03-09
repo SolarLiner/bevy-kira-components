f2cddf07e947ed3551e6d304bf498b9a46520379
Nathan Graule <nathan.graule@arturia.com>
2024-03-09 23:37:37 +0100

    feat: add diagnostics for num spatial scenes

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -1,6 +1,6 @@
+use crate::AudioWorld;
 use bevy::diagnostic::{Diagnostic, DiagnosticPath, Diagnostics, RegisterDiagnostic};
 use bevy::prelude::*;
-use crate::AudioWorld;
 
 pub struct KiraStatisticsDiagnosticPlugin;
 
@@ -10,6 +10,7 @@ impl Plugin for KiraStatisticsDiagnosticPlugin {
             .register_diagnostic(Diagnostic::new(NUM_SOUNDS).with_suffix(" sounds"))
             .register_diagnostic(Diagnostic::new(NUM_CLOCKS).with_suffix(" clocks"))
             .register_diagnostic(Diagnostic::new(NUM_MODULATORS).with_suffix(" modulators"))
+            .register_diagnostic(Diagnostic::new(NUM_SPATIAL_SCENES).with_suffix(" spatial_scenes"))
             .add_systems(Last, record_diagnostics);
     }
 }
@@ -18,6 +19,7 @@ const NUM_COMMANDS: DiagnosticPath = DiagnosticPath::const_new("kira::manager::n
 const NUM_SOUNDS: DiagnosticPath = DiagnosticPath::const_new("kira::manager::num_sounds");
 const NUM_CLOCKS: DiagnosticPath = DiagnosticPath::const_new("kira::manager::num_clocks");
 const NUM_MODULATORS: DiagnosticPath = DiagnosticPath::const_new("kira::manager::num_modulators");
+const NUM_SPATIAL_SCENES: DiagnosticPath = DiagnosticPath::const_new("kira::manager::num_spatial_scenes");
 
 fn record_diagnostics(audio_world: Res<AudioWorld>, mut diagnostics: Diagnostics) {
     diagnostics.add_measurement(&NUM_COMMANDS, || {
@@ -28,4 +30,5 @@ fn record_diagnostics(audio_world: Res<AudioWorld>, mut diagnostics: Diagnostics
     diagnostics.add_measurement(&NUM_MODULATORS, || {
         audio_world.audio_manager.num_modulators() as _
     });
+    diagnostics.add_measurement(&NUM_SPATIAL_SCENES, || audio_world.audio_manager.num_spatial_scenes() as _);
 }
