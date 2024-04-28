3864d282a56dbe41ccc932bfd48dae479c2464e8
Nathan Graule <solarliner@gmail.com>
2024-04-28 11:26:44 +0200

    feat: remove tracks integration
    It's clunky and does not work properly, requires too much fiddling with builders and spawning, I don't like it :(

--- examples/interactive/src/main.rs
+++ examples/interactive/src/main.rs
@@ -1,24 +1,21 @@
-mod ui;
-
 use bevy::prelude::*;
 use bevy::utils::error;
 
-use crate::ui::UiPlugin;
 use bevy_kira_components::kira::sound::Region;
-use bevy_kira_components::kira::track::effect::panning_control::{
-    PanningControlBuilder, PanningControlHandle,
-};
-use bevy_kira_components::kira::track::TrackBuilder;
 use bevy_kira_components::kira::tween::Tween;
 use bevy_kira_components::prelude::AudioBundle;
 use bevy_kira_components::prelude::*;
 use diagnostics_ui::DiagnosticsUiPlugin;
 
+use crate::ui::UiPlugin;
+
+mod ui;
+
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, AudioPlugin, DiagnosticsUiPlugin, UiPlugin))
         .add_systems(Startup, init)
-        .add_systems(Update, (handle_interactive_sound, update_track_panning))
+        .add_systems(Update, handle_interactive_sound)
         .run();
 }
 
@@ -31,11 +28,6 @@ struct PanTrack;
 fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle { ..default() });
     let audio_file = asset_server.load::<AudioFile>("drums.ogg");
-    let mut track = TrackBuilder::new();
-    let panning = track.add_effect(PanningControlBuilder::default());
-    let track_entity = commands
-        .spawn((Track(track), EffectHandle(panning), PanTrack))
-        .id();
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_scale(Vec3::splat(25.0)),
@@ -52,24 +44,10 @@ fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
                 start_paused: true,
                 ..default()
             },
-            output: OutputDestination::SpecificTrack(track_entity),
             ..default()
         },
         InteractiveSound,
     ));
-}
-
-fn update_track_panning(
-    time: Res<Time>,
-    mut q: Query<&mut EffectHandle<PanningControlHandle>, With<PanTrack>>,
-) {
-    let pan = time.elapsed_seconds_f64().sin();
-    let pan = 0.5 * pan + 0.5;
-    for mut effect in &mut q {
-        if let Err(err) = effect.set_panning(pan, Tween::default()) {
-            error!("Cannot update track panning: {err}");
-        }
-    }
 }
 
 fn handle_interactive_sound(
