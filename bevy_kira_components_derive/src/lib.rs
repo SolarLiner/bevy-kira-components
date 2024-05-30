d365dca4ec4d9fc1ccf653c9fbcba232f0d1888c
GitGhillie <jillisnoordhoek@gmail.com>
2024-05-30 23:48:44 +0200

    Use visitor pattern in effect rack macro

--- bevy_kira_components_derive/src/lib.rs
+++ bevy_kira_components_derive/src/lib.rs
@@ -2,7 +2,8 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::{format_ident, quote};
 use syn;
-use syn::{Data, Fields, Ident, Type};
+use syn::visit::Visit;
+use syn::{visit, Field, Ident, PathSegment};
 
 #[proc_macro_derive(EffectRack)]
 pub fn create_derive(input: TokenStream) -> TokenStream {
@@ -11,58 +12,46 @@ pub fn create_derive(input: TokenStream) -> TokenStream {
     impl_create(&ast)
 }
 
+struct DeriveVisitor {
+    field_name_idents: Vec<Ident>,
+    handle_type_idents: Vec<Ident>,
+}
+
+impl<'ast> Visit<'ast> for DeriveVisitor {
+    fn visit_field(&mut self, node: &'ast Field) {
+        // todo: handle unnamed fields (tuple structs)
+        self.field_name_idents.push(node.ident.clone().unwrap());
+        visit::visit_field(self, node);
+    }
+
+    fn visit_path_segment(&mut self, node: &'ast PathSegment) {
+        let ident_string = node.ident.to_string();
+        let stripped = ident_string
+            .strip_suffix("Builder")
+            .expect("All types in an EffectRack must be effect builders ending with 'Builder'.");
+
+        let type_ident = format_ident!("{}Handle", stripped);
+        self.handle_type_idents.push(type_ident);
+        visit::visit_path_segment(self, node);
+    }
+}
+
 fn impl_create(ast: &syn::DeriveInput) -> TokenStream {
+    // Name of the effect rack type
     let rack_ident = &ast.ident;
-    let data = &ast.data;
 
-    let mut fields = &Fields::Unit;
-
-    match data {
-        Data::Struct(data_struct) => fields = &data_struct.fields,
-        _ => {}
-    }
-
-    let mut builder_type_idents: Vec<&Ident> = Vec::new();
-    let mut field_name_idents: Vec<&Ident> = Vec::new();
-
-    // todo: Take tuple structs into account
-    // todo: Panic messages
-    match fields {
-        Fields::Named(fields_named) => {
-            for field in &fields_named.named {
-                if let Some(ident) = &field.ident {
-                    field_name_idents.push(ident);
-                } else {
-                    panic!();
-                }
-
-                match &field.ty {
-                    Type::Path(type_path) => {
-                        for segment in &type_path.path.segments {
-                            builder_type_idents.push(&segment.ident);
-                        }
-                    }
-                    _ => {}
-                }
-            }
-        }
-        _ => {}
-    }
-
-    let handle_type_idents: Vec<Ident> = builder_type_idents
-        .iter()
-        .map(|ident| {
-            let string = ident.to_string();
-            let stripped = string.strip_suffix("Builder").expect(
-                "All types in an EffectRack must be effect builders ending with 'Builder'.",
-            );
-
-            format_ident!("{}Handle", stripped)
-        })
-        .collect();
+    // Collect all the Fields
+    let mut visitor = DeriveVisitor {
+        field_name_idents: vec![],
+        handle_type_idents: vec![],
+    };
+    visitor.visit_derive_input(&ast);
 
     let controller_ident = Ident::new(&format!("{}Controller", rack_ident), Span::call_site());
+    let field_name_idents = visitor.field_name_idents;
+    let handle_type_idents = visitor.handle_type_idents;
 
+    // Generate the effect rack controller and the associated implementation
     let gen = quote! {
         #[derive(Component)]
         struct #controller_ident {
